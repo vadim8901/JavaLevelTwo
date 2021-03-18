be73ae8bd7e9b8ac5938d7f35163f795da2443b1
vadim8900 <vadim-gryaznykh@yandex.ru>
2021-03-18 18:08:36 +0300

    lesson1

--- src/lesson1/Cat.java
+++ src/lesson1/Cat.java
@@ -1,6 +1,15 @@
 package lesson1;
 
-public class Cat {
+public class Cat implements Jump, Run{
 
 
+    @Override
+    public void jump() {
+        System.out.println("Cat jump");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Cat run");
+    }
 }
---
+++ src/lesson1/Jump.java
@@ -0,0 +1,5 @@
+package lesson1;
+
+public interface Jump {
+    void jump();
+}
